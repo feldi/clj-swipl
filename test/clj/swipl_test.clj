2ffd0a0fd0da62ee6ff3ccea83a406f7d218472b
feldi <peter.feldtmann@gmail.com>
2014-06-29 13:26:34 +0200

    added tests

--- test/clj/swipl_test.clj
+++ test/clj/swipl_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [clj.swipl :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest jpl-tests
+  
+  (testing "version"
+           (is (= (get-version-as-string) "3.1.4-alpha")))
+  
+  (testing "prolog lists"
+           (let [pl-list (seq-to-pl-list [(make-atom "a") (make-atom "b") (make-atom "c")])
+                 clj-list (pl-list-to-clj-list pl-list)
+                 clj-vec (pl-list-to-vec pl-list)]
+             (is (= '("a" "b" "c") clj-list))
+             (is (= ["a" "b" "c"] clj-vec))))
+  
+  (testing "prolog exception"
+           (is (thrown? jpl.PrologException (make-query-from-text "p(].")))
+           )
+  )
